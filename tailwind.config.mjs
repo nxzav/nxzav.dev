49b44f73ac39c4f6b8080c4d246850f07b8f84fd
N Zavala <noezh.25@gmail.com>
2024-06-03 05:53:42 -0600

    Mouse track animation, font update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-				jetbrains: ['JetBrains Mono', 'monospace', 'sans-serif']
+        roboto: ["Roboto", "sans-serif"],
+        jetbrains: ["JetBrains Mono", "monospace", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
